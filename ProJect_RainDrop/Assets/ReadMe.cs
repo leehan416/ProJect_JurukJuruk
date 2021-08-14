d86beee37acbf23f0ff9430a8ad497873ea3ebc2
Whal3 <35258105+leehan416@users.noreply.github.com>
2021-08-15 01:33:55 +0900

    Update ReadMe.cs

--- ProJect_RainDrop/Assets/ReadMe.cs
+++ ProJect_RainDrop/Assets/ReadMe.cs
@@ -1,25 +1,5 @@
 ﻿/*
 
-project design => developing(now) =>alpha => beta => debugging => 1.0
-
-TODO
-1. main => extra pot set(UI, System) + Charactor Set
-2. Cleaning =>  System set, Up System set
-3. Map =>  System Set
-4. Market => UI set, Up System set, pot up System set
-5. Setting => System Set
-6. Shop => UI Set Customer Data Set, sell System set 
-
-
-
-
-SCRIPTS SET
-1. main => 
-2. Cleaning => 
-3. Map => 
-4. Market => 
-5. Setting =>
-6. Shop =>  
-
+project design => developing =>alpha(now) => beta => debugging => 1.0
 
 */
