760c5fe945349aefdbbfe5f59d180f0dc588292c
leehan416 <leehan416@naver.com>
2021-08-25 20:10:41 +0900

    8/25
    로고씬 제작

---
+++ ProJect_RainDrop/Assets/Scripts/UI_Scripts/UI_LogoScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_LogoScene : MonoBehaviour {
+    private void Start()
+    {
+        StartCoroutine(move());
+    }
+
+
+    IEnumerator move()
+    {
+        yield return new WaitForSeconds(1f);
+        GameObject.Find("Canvas/LOGO").SetActive(false);
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("Intro");
+    }
+}
